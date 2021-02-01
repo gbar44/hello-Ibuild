cd6eb6f087fa7c838e0bcef83543dcbe54a1857f
gbar44 <73077055+gbar44@users.noreply.github.com>
2021-02-01 11:14:16 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function doit(){}
+
+function addone(){
+   var foo = document.getElementById('thisone').innerHTML;
+   foo++;
+   document.getElementById('thisone').innerHTML = foo;
+}
+
+  
+   
+
+function add(){
+   var bill = document.getElementById('this').innerHTML;
+   bill++;
+   document.getElementById('this').innerHTML = bill;
+}
+
+function addon(){
+   var joe = document.getElementById('thisme').innerHTML;
+   joe++;
+   document.getElementById('thisme').innerHTML = joe;
+} 
+
